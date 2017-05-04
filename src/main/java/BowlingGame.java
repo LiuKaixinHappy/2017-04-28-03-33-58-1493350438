f5ceee71a80db46a652a93f21f60297a51ce8739
liukx <644306737@qq.com>
2017-05-04 09:34:43 +0800

    新增 有额外球情况下的分数计算

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -5,16 +5,154 @@ public class BowlingGame {
 
     public int getBowlingScore(String bowlingCode) {
         List<Frame> frames = createFrame(bowlingCode);
+        int score = 0;
 
-
-        int score = 300;
-
+        for (Frame frame : frames) {
+            if (frame.getNext_first().equals("/")) {
+                score += 10;
+                score += Integer.valueOf(frame.getNext_second());
+            } else {
+                if (frame.getSelf().equals("10")) {
+                    score += 10;
+                    if (frame.getNext_second().equals("/")) {
+                        score += 10;
+                    } else {
+                        score += Integer.valueOf(frame.getNext_first());
+                        score += Integer.valueOf(frame.getNext_second());
+                    }
+                } else {
+                    score += Integer.valueOf(frame.getSelf());
+                    score += Integer.valueOf(frame.getNext_first());
+                }
+            }
+        }
         return score;
     }
 
     private List<Frame> createFrame(String bowlingCode) {
         List<Frame> frames = new ArrayList<Frame>();
-//        String frames[] = bowlingCode.split("\\|");
+        String str[] = bowlingCode.split("\\|");
+        // 有额外球
+        if (str.length == 12) {
+            for (int i = 0; i < str.length - 3; i++) {
+                String self;
+                String next_first;
+                String next_second;
+                if (str[i].length() == 1) {
+                    self = "X";
+                    if (str[i + 1].length() == 1) {
+                        next_first = "X";
+                        if (i == 8) {
+                            next_second = str[11].substring(0, 1);
+                        } else {
+                            next_second = str[i + 2].substring(0, 1);
+                        }
+                    } else {
+                        next_first = str[i + 1].substring(0, 1);
+                        next_second = str[i + 1].substring(1, 2);
+                    }
+                    if (self.equals("X")) {
+                        self = "10";
+                    }
+                    if (next_first.equals("X")) {
+                        next_first = "10";
+                    }
+                    if (next_second.equals("X")) {
+                        next_second = "10";
+                    }
+                    if (self.equals("-")) {
+                        self = "0";
+                    }
+                    if (next_first.equals("-")) {
+                        next_first = "0";
+                    }
+                    if (next_second.equals("-")) {
+                        next_second = "0";
+                    }
+                    frames.add(new Frame(self, next_first, next_second));
+                } else {
+                    self = str[i].substring(0, 1);
+                    next_first = str[i].substring(1, 2);
+                    next_second = str[i + 1].substring(0, 1);
+                    if (self.equals("X")) {
+                        self = "10";
+                    }
+                    if (next_first.equals("X")) {
+                        next_first = "10";
+                    }
+                    if (next_second.equals("X")) {
+                        next_second = "10";
+                    }
+                    if (self.equals("-")) {
+                        self = "0";
+                    }
+                    if (next_first.equals("-")) {
+                        next_first = "0";
+                    }
+                    if (next_second.equals("-")) {
+                        next_second = "0";
+                    }
+                    frames.add(new Frame(self, next_first, next_second));
+                }
+            }
+            if (str[9].length() == 1) {
+                String self = "X";
+                String next_first;
+                String next_second;
+                if (str[11].length() == 1) {
+                    next_first = str[11];
+                    next_second = "0";
+                } else {
+                    next_first = str[11].substring(0, 1);
+                    next_second = str[11].substring(1, 2);
+                }
+                if (self.equals("X")) {
+                    self = "10";
+                }
+                if (next_first.equals("X")) {
+                    next_first = "10";
+                }
+                if (next_second.equals("X")) {
+                    next_second = "10";
+                }
+                if (self.equals("-")) {
+                    self = "0";
+                }
+                if (next_first.equals("-")) {
+                    next_first = "0";
+                }
+                if (next_second.equals("-")) {
+                    next_second = "0";
+                }
+                frames.add(new Frame(self, next_first, next_second));
+            } else {
+                String self = str[9].substring(0, 1);
+                String next_first = str[9].substring(1, 2);
+                String next_second = str[11].substring(0, 1);
+                if (self.equals("X")) {
+                    self = "10";
+                }
+                if (next_first.equals("X")) {
+                    next_first = "10";
+                }
+                if (next_second.equals("X")) {
+                    next_second = "10";
+                }
+                if (self.equals("-")) {
+                    self = "0";
+                }
+                if (next_first.equals("-")) {
+                    next_first = "0";
+                }
+                if (next_second.equals("-")) {
+                    next_second = "0";
+                }
+                frames.add(new Frame(self, next_first, next_second));
+            }
+        } else {
+
+        }
+
         return frames;
     }
 
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -7,26 +7,33 @@ public class BowlingGameTest {
     @Test
     public void test_all_strike() throws Exception {
         String bowlingCode = "X|X|X|X|X|X|X|X|X|X||XX";           //300
-        assertEquals(new BowlingGame().getBowlingScore(bowlingCode), 300);
+        assertEquals(300,  new BowlingGame().getBowlingScore(bowlingCode));
     }
 
     @Test
     public void test_second_spare() throws Exception {
         String bowlingCode = "5/|5/|5/|5/|5/|5/|5/|5/|5/|5/||5";  //150
-        assertEquals(new BowlingGame().getBowlingScore(bowlingCode), 150);
+        assertEquals(150, new BowlingGame().getBowlingScore(bowlingCode));
     }
 
     @Test
     public void test_second_miss() throws Exception {
         String bowlingCode = "9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||";   //90
-        assertEquals(new BowlingGame().getBowlingScore(bowlingCode), 90);
+        assertEquals(90, new BowlingGame().getBowlingScore(bowlingCode));
 
     }
 
     @Test
     public void test_other_situation() throws Exception {
         String bowlingCode = "X|7/|9-|X|-8|8/|-6|X|X|X||81";      //167
-        assertEquals(new BowlingGame().getBowlingScore(bowlingCode), 167);
+        assertEquals(167, new BowlingGame().getBowlingScore(bowlingCode));
+
+    }
+
+    @Test
+    public void test_0_situation() throws Exception {
+        String bowlingCode = "--|--|--|--|--|--|--|--|--|X||81";      //19
+        assertEquals(19, new BowlingGame().getBowlingScore(bowlingCode));
 
     }
 }
