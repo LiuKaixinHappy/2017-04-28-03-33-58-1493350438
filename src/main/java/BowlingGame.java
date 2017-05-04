df69fcd43cb26c6b62595e0c8f0ac79d18c6cc32
liukx <644306737@qq.com>
2017-05-04 16:37:58 +0800

    重构 添加注释

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -12,6 +12,12 @@ public class BowlingGame {
         return score;
     }
 
+    /**
+     * 计算每一格的分数.
+     *
+     * @param frame 格
+     * @return 分数.
+     */
     private int calculateFrameScore(Frame frame) {
         if (isSpare(frame.getNext_first())) {
             return 10 + Integer.valueOf(frame.getNext_second());
@@ -20,23 +26,42 @@ public class BowlingGame {
             if (isSpare(frame.getNext_second())) {
                 return 20;
             }
-            return 10 + Integer.valueOf(frame.getNext_first()) + Integer.valueOf(frame.getNext_second());
+            return 10 + Integer.valueOf(frame.getNext_first())
+                    + Integer.valueOf(frame.getNext_second());
         }
-        return Integer.valueOf(frame.getSelf()) + Integer.valueOf(frame.getNext_first());
+        return Integer.valueOf(frame.getSelf())
+                + Integer.valueOf(frame.getNext_first());
     }
 
+    /**
+     * 判断是否是strike.
+     *
+     * @param string 字符串.
+     * @return true or false.
+     */
     private boolean isStrike(String string) {
         return string.equals("10");
     }
 
+    /**
+     * 判断是否是spare.
+     *
+     * @param string 字符串.
+     * @return true or false.
+     */
     private boolean isSpare(String string) {
         return string.equals("/");
     }
 
+    /**
+     * 创建格子列表,每个格子包含当前球分数和接下来两颗球的分数.
+     *
+     * @param bowlingCode 保龄球字串.
+     * @return 格子列表.
+     */
     private List<Frame> createFrame(String bowlingCode) {
         List<Frame> frames = new ArrayList<>();
         String str[] = bowlingCode.split("\\|");
-        // 遍历除了最后一颗球之外的球
         for (int i = 0; i < 10; i++) {
             String self = setSelf(str[i]);
             String next_first;
@@ -55,6 +80,12 @@ public class BowlingGame {
         return frames;
     }
 
+    /**
+     * 将spare和strike转为10和0.
+     *
+     * @param str 字符串.
+     * @return spare：0，strike：10， others：self.
+     */
     private String toNum(String str) {
         if (str.equals("X")) {
             return "10";
@@ -63,6 +94,17 @@ public class BowlingGame {
             return "0";
         }
         return str;
+    }
+
+    private String setSelf(String self) {
+        return self.substring(0, 1);
+    }
+
+    private String setNextFirst(String self, String nextFirst) {
+        if (self.length() == 2) {
+            return self.substring(1, 2);
+        }
+        return nextFirst.substring(0, 1);
     }
 
     private String setNextSecond(String self, String nextFirst, String nextSecond, String nextThird) {
@@ -75,17 +117,9 @@ public class BowlingGame {
         return nextFirst.substring(1, 2);
     }
 
-    private String setNextFirst(String self, String nextFirst) {
-        if (self.length() == 2) {
-            return self.substring(1, 2);
-        }
-        return nextFirst.substring(0, 1);
-    }
-
-    private String setSelf(String self) {
-        return self.substring(0, 1);
-    }
-
+    /**
+     * 格子类，每个格子包含当前球分数和接下来两颗球的分数.
+     */
     private class Frame {
         private String self;
         private String next_first;
